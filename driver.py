985dbbb7be2c393b26ccd8ec310416a05b9459e1
kimth <kimth@stanford.edu>
2014-03-11 15:35:51 +0000

    Prepare for batch processing

--- driver.py
+++ driver.py
@@ -3,23 +3,47 @@ from ann import BasicNetwork
 import activation_functions as af
 import mnist_loader
 
+import math
+
 # Load data (see data/README for instructions on downloading MNIST set)
 training_data, validation_data, test_data = \
     mnist_loader.load_data_wrapper()
     
-# Create a basic three-layer network with [784, 30, 10] neurons
-num_neurons = [784, 30, 10]
 
-# By default, activation function is sigmoid
-#net = BasicNetwork(num_neurons)
+# Parameters of ANN
+# - af_name: Activation function of neurons (see activation_functions.py)
+# - epochs: Number of full passes over training data
+# - mini_batch_size: Number of samples for a single SGD step
+# - eta: Learning rate
+params = {'layers': [784, 30, 10],
+          'af_name': 'sigmoid',
+          'epochs': 3,
+          'mini_batch_size': 10,
+          'eta': 0.5,
+         }
 
-# Instead, we can use the tanh function
-net = BasicNetwork(num_neurons, f=af.tanh_vec, fp=af.tanh_prime_vec)
+# Instantiate the network
+net = BasicNetwork(params['layers'],
+                   f =af.functions[params['af_name']][0],
+                   fp=af.functions[params['af_name']][1])
 
-# Have the basic network learn to classify the MNIST dataset, by 
+print "# Instantiated ANN:\n\
+#   - Structure: {} neurons\n\
+#   - Activation function: {}".format(params['layers'], params['af_name'])
+
+print "# Running SGD:\n\
+#   - Epochs: {}\n\
+#   - Mini-batch size: {} per batch ({} batches)".format(
+        params['epochs'], params['mini_batch_size'],
+        len(training_data)/params['mini_batch_size'])
+print "#------------------------------------------------------------"
+
+# Have the basic network learn to classify the MNIST dataset, by
 # running the following
-net.SGD(training_data,
-        epochs=30,          # Number of full passes over training data
-        mini_batch_size=10, # Number of samples for a single SGD step
-        eta = 0.5,          # Learning rate
-        test_data = test_data)
+test_results = net.SGD(training_data,
+                       epochs = params['epochs'],
+                       mini_batch_size = params['mini_batch_size'],
+                       eta = params['eta'],
+                       test_data = test_data)
+
+print test_results
