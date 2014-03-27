c23b661e74adf2b84ef8bcd4b05c91a25e6e766f
kimth <kimth@stanford.edu>
2014-03-27 11:09:33 -0700

    Optional loading of multiple classes

--- driver_oja.py
+++ driver_oja.py
@@ -7,12 +7,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Pull all training examples for a given label
-label = 8
+label = 3
 training_data = mnist_loader.load_training_data_with_label(label)
+
+# Optional loading of multiple classes
+td2 = mnist_loader.load_training_data_with_label(7)
+training_data.extend(td2)
 
 # Run a single pass through data
 net = OjaNetwork(784)
-net.learn_oja(training_data, 0.01)
+net.learn_oja(training_data, 0.02)
 
 # Show the result
 plt.imshow(np.reshape(net.weights, (28,28)), cmap=cm.Greys_r)
